2333a5c99aa163944dbe0ee794e18ee94367bbe3
LouetR <romainlouet@romsworld.net>
2021-08-28 19:53:24 +0200

    use paramconverter

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Rest\Route("/api")
@@ -15,14 +16,15 @@ class UserController extends AbstractFOSRestController
 {
     /**
      * @Rest\Get(
-     *     path = "/company/{id}/user",
+     *     path = "/company/{company_id}/user",
      *     name = "app_users_list",
-     *     requirements = {"id"="\d+"}
+     *     requirements = {"company_id"="\d+"}
      * )
      * @Rest\View(
      *     serializerGroups = {"USER_LIST"}
      * )
      */
+    #[ParamConverter("company", options: ['mapping' => ['company_id' => 'id']])]
     public function getUsersList(Company $company, UserRepository $userRepository)
     {
         $users = $userRepository->findBy(['company' => $company], ['lastName' => 'ASC', 'firstName' => 'ASC']);
