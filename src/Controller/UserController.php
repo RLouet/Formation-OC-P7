c82c59a88e65cc5185f6350b3c1105d22673bfa1
LouetR <romainlouet@romsworld.net>
2021-08-29 19:07:25 +0200

    add location header to the response

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,7 +9,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * @Rest\Route("/api")
@@ -76,6 +78,10 @@ class UserController extends AbstractFOSRestController
         ;
         $entityManager->persist($user);
         $entityManager->flush();
-        return $user;
+        return $this->view(
+            $user,
+            Response::HTTP_CREATED,
+            ['Location' => $this->generateUrl('app_user_show', ['company_id' => $user->getCompany()->getId(), 'user_id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL)]
+        );
     }
 }
