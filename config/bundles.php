e4a6219e7622d28ab8004424f4965439d3a95dbd
LouetR <romainlouet@romsworld.net>
2021-08-31 15:58:02 +0200

    install pagerfanta bundle, create pagination abstract repository

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
