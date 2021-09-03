9f030db1b0e6d803dcb89c25ff2e3e2285060c66
LouetR <romainlouet@romsworld.net>
2021-09-03 17:06:54 +0200

    view exceptions in json format, add RessourceValidationException exception

---
+++ src/Exception/RessourceValidationException.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
+
+class RessourceValidationException extends HttpException
+{
+    private array $errors;
+    public function __construct()
+    {
+        $message = "The JSON sent contains invalid data. Here are the errors you need to correct:";
+        parent::__construct("400", $message);
+    }
+
+    public function addError(string $field, string $message)
+    {
+        $this->errors[] = [
+            "field" => $field,
+            "message" => $message
+        ];
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+}
